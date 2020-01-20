598fe1c840c67dba841c38af4ddce1db5700dea1
JerryVW <jerryvw72@gmail.com>
2020-01-20 12:34:53 -0500

    Finished functionality of javscript and cleaned up unused HTML, CSS and Javascript code

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+window.onload = doItNow;
+
 let form = document.getElementById("add-form");
 let itemList = document.getElementById("items");
 
-// Add the ToDo element
-document.getElementById("add-item").addEventListener("click", addItem);
-document.querySelector("ul").addEventListener("click", removeOrChecked);
-
+function doItNow() {
+    document.getElementById("add-item").addEventListener("click", addItem);
+    document.querySelector("ul").addEventListener("click", removeOrChecked);
+}
 
 // Add item function
 function addItem(e) {
     // In this function we get the user input, create the new element
-    // and display the TODO item in the new element
+    // and display the TODO item in the list.
     e.preventDefault();
     let newItem = document.getElementById("item").value;
     let li = document.createElement("li");
     li.innerHTML = `<button class="not-checked"></button><div class="list-item">${newItem}</div><button class="to-delete">X</button>`;
     itemList.appendChild(li);
+    newItem = document.getElementById("item").value = "";
 } 
 
 function removeOrChecked(e) {
@@ -26,9 +29,9 @@ function removeOrChecked(e) {
 }
 
 function removeItem(e) {
-    // In this function we will remove an item from the list
+    // In this function we will remove an item from the list.
     if(e.target.classList.contains("to-delete")){
-        let li = e.target.parentElement;
+        const li = e.target.parentElement;
         itemList.removeChild(li);
     }
     
@@ -36,10 +39,9 @@ function removeItem(e) {
 
 function checkedOff(e) {
     // In this function we will change the check box color
-    // and add a strikeThrough to the text
+    // and add a strikeThrough to the text.
     if(e.target.classList.contains("not-checked")) {
-        let li = e.target.innerHTML;
-        //li.style.textDecoration = "line-through";
-        console.log(li);
+        const li = e.target.nextSibling.style.textDecoration = "line-through";
+        li = e.target.style.backgroundColor = "rgb(0, 255, 13)";
     }
 }
