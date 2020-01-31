cb03425c7479ee61beafc6d874884df8dd18e9ef
Jerry Vander Weide <59615980+JerryVW@users.noreply.github.com>
2020-01-31 10:14:53 -0500

    Revised code for better readability.

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
-window.onload = doItNow;
-
-const listArray = JSON.parse(localStorage.getItem("list-item")) || [];
-
-function doItNow() {
+window.onload = function(e) {
     document.getElementById("add-item").addEventListener("click", addItem);
-    document.querySelector("ul").addEventListener("click", removeOrChecked);
+    const listArray = JSON.parse(localStorage.getItem("list-item")) || [];
     listArray.forEach(function(item) {
         createListItem(item);
     });
-}
-
-function createListItem(newItem) {
-    let itemList = document.getElementById("items");
-    const li = document.createElement("li");
-    const input = document.createElement("input");
-    input.type = "checkbox";
-    input.className = "not-checked";
-    li.appendChild(input);
-    const div = document.createElement("div");
-    div.className = "list-item";
-    div.textContent = newItem;
-    li.appendChild(div);
-    const button = document.createElement("button");
-    button.className = "to-delete";
-    button.textContent = "X";
-    li.appendChild(button);
-    itemList.appendChild(li);
-} 
+};
 
 function addItem(e) {
     e.preventDefault();
     let newItem = document.getElementById("item").value;
     createListItem(newItem); 
+    const listArray = JSON.parse(localStorage.getItem("list-item")) || [];
     listArray.push(newItem);
     localStorage.setItem("list-item", JSON.stringify(listArray));
     newItem = document.getElementById("item").value = "";
 }
 
-function removeOrChecked(e) {
-    if(e.target.className === "to-delete"){
-        removeItem(e);
-    } else {
-        checkedOff(e);
-    }
+function createListItem(newItem) {
+    let itemList = document.getElementById("items");
+    const li = document.createElement("li");
+
+    li.appendChild(createCheckBox());
+    li.appendChild(createTextDiv(newItem));
+    li.appendChild(createDeleteButton());
+    itemList.appendChild(li);
+} 
+
+function createCheckBox() {
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "not-checked";
+    checkbox.addEventListener("click", function(e) {
+        if(e.target.classList.contains("not-checked")) {
+            if(e.target.nextSibling.style.textDecoration !== "line-through") {
+                e.target.nextSibling.style.textDecoration = "line-through";
+            } else {
+                e.target.nextSibling.style.textDecoration = "none";
+            }
+        }
+    })
+
+    return checkbox;
 }
 
-function removeItem(e) {
-    if(e.target.classList.contains("to-delete")){
-        let itemList = document.getElementById("items");
-        const li = e.target.parentElement;
-        itemList.removeChild(li);
-        let arrayList = listArray.indexOf(e.target.previousSibling.textContent);
-        listArray.splice(arrayList, 1);
-        localStorage.setItem("list-item", JSON.stringify(listArray));
-    };
+function createTextDiv(newItem) {
+    const div = document.createElement("div");
+    div.className = "list-item";
+    div.textContent = newItem;
+
+    return div;
 }
 
-function checkedOff(e) {
-    if(e.target.classList.contains("not-checked")) {
-        let li = e.target.nextSibling.style.textDecoration = "line-through";
-    }
+function createDeleteButton() {
+    const button = document.createElement("button");
+    button.className = "to-delete";
+    button.textContent = "X";
+    button.addEventListener("click", function(e) {
+        if(e.target.classList.contains("to-delete")){
+            let itemList = document.getElementById("items");
+            const li = e.target.parentElement;
+            itemList.removeChild(li);
+            const listArray = JSON.parse(localStorage.getItem("list-item")) || [];
+            let arrayList = listArray.indexOf(e.target.previousSibling.textContent);
+            listArray.splice(arrayList, 1);
+            localStorage.setItem("list-item", JSON.stringify(listArray));
+        }
+    });
+
+    return button;
 }
